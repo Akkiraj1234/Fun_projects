e84f391049761964fc05c58a9148630a21489569
Akkiraj1234 <141214556+Akkiraj1234@users.noreply.github.com>
2024-10-08 10:00:35 +0530

    added kbc-game repo inside Fun project

---
+++ kbc-game/kbc game.py
@@ -0,0 +1,322 @@
+list2=[
+' who is known as father of chemistry?',
+' Air pressure is related to densityy of air. this was discoverd by whom in 1662 AD?',
+' the city which has the highest sulphure dioxide pollution is-/',
+' pollution control reserch institute is located at ?',
+' Nuclear radiation can cause n eye disease called?',
+' in a human baby,teethin process start from the age?',
+' which material is used to make cellophane?',
+' plaster of paris is obtained when?',
+' NMR stand for?',
+' The formation of a blood clot within a blood vessel is called?',
+' What is the national flower of India?',
+' Which planet is known as the Red Planet?',
+' Who is known as the "Father of the Nation" in India?',
+' Which gas do plants absorb from the atmosphere during photosynthesis?',
+' What is the capital of France?',
+' What is the largest mammal in the world?',
+' Who wrote the play "Romeo and Juliet"?',
+' What is the chemical symbol for gold?',
+' What is the tallest mountain in the world?',
+' Which gas do humans breathe out?',
+' Who painted the "Mona Lisa"?',
+' What is the largest planet in our solar system?',
+' Which country is known as the Land of the Rising Sun?',
+' What is the smallest prime number?',
+' Who was the first woman to fly solo across the Atlantic Ocean?',
+' In which year did Christopher Columbus first reach the Americas?',
+' What is the largest organ in the human body?',
+' Who wrote the novel "To Kill a Mockingbird"?',
+' What is the chemical symbol for oxygen?',
+' Who is the author of "Harry Potter and the Sorcerer is Stone"?',
+' Typhoide vaccine was invented by ?',
+' Velocity of light is maximum in ?',
+' When sky is seen from an aeroplane at high altitude it appears?',
+' which instrument is used for the measurment of humidity in air?',
+' The primary colors in photography are ?',
+' which one is not fat-soluble vitamin?',
+' who wowrks closely in the body with vitamin B12',
+' which is not viral disease?',
+' The IC chip used in computers is maded of?',
+' Hair is composed of protein called ?',
+' Viticulture is known for the production of?',
+' the first nuclear explosion in india was conducted at ?',
+' fat is completely digested in the?',
+' which hormone controls the growth of plants?',
+' how many ventricles are there in humans brain?',
+' breating roots are seen in? ',
+' state funding of election takes place in ?',
+' the concept of PIL originated in ?',
+' when was the national emergency declared for the first time on the basic of internal disturbance ?',
+' when did the principle of judicial activism become popular ?',
+' the state reorganisation act was passed in the year ?',
+]
+kbc_questions =[
+    ['Lavoiser','Mendelieve','Rutherford','pauli','Lavoiser'],
+    ['Daniel Charles','Robert Boyle','Blaise pascal','robert Grahm','Robert Boyle'],
+    ['Mumbai','london','nerlin','milan','milan'],
+    ['shimla','haridwar','Chandigarh','Dehradun','haridwar'],
+    ['Cataract','glaucoma','Retinopathy','Corneal distraction','Cataract'],
+    ['Four months','Six months','Seven months','Nine months','Seven months'],
+    ['phenol','Benzene','Cellulose','Vinyl Chloride','Cellulose'],
+    ['gypsum is heated to 80*C','gypsum is heated in blast funnace','gpsum is hydrated with tiwise quantity of water','gypsum is heated to 120*C','gypsum is heated to 120*C'],
+    ['Neuron Microscopic restructuring','Neuron mind Resteucturing','Neuron magnetic Resonance','nuclear Magnetic Rescouse','nuclear Magnetic Rescouse'],
+    ['Thrombosis','Atherosclerosis','Reticulosis','Cholestrimia','Thrombosis'],
+    ["Lotus", "Rose", "Sunflower", "Tulip", "Lotus"],
+    ["Venus", "Mars", "Jupiter", "Saturn", "Mars"],
+    ["Jawaharlal Nehru", "Sardar Patel", "Mahatma Gandhi", "Subhas Chandra Bose", "Mahatma Gandhi"],
+    ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen", "Carbon Dioxide"],
+    ["London", "Rome", "Paris", "Madrid", "Paris"],
+    ["Blue Whale", "African Elephant", "Giraffe", "Polar Bear", "African Elephant"],
+    ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain", "William Shakespeare"],
+    ["Au", "Ag", "Fe", "Cu", "Ag"],
+    ["Mount Everest", "K2", "Kilimanjaro", "Mount Fuji", "Mount Everest"],
+    ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen", "Carbon Dioxide"],
+    ["Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso", "Michelangelo", "Vincent van Gogh"],
+    ["Jupiter", "Saturn", "Earth", "Neptune", "Saturn"],
+    ["Japan", "China", "South Korea", "Thailand", "Japan"],
+    ["1", "2", "3", "5", "1"],
+    ["Amelia Earhart", "Bessie Coleman", "Harriet Quimby", "Jacqueline Cochran", "Amelia Earhart"],
+    ["1492", "1498", "1505", "1512", "1492"],
+    ["Skin", "Liver", "Heart", "Brain", "Skin"],
+    ["Harper Lee", "F. Scott Fitzgerald", "J.D. Salinger", "John Steinbeck","Harper Lee"],
+    ["O", "N", "H", "He", "O"],
+    ["J.K. Rowling", "Chinua Achebe", "George Orwell", "Amit Chauduri", "J.K. Rowling"],
+    ['catatact','Ppressbyopia','Glaucoma','Astigmatism','Astigmatism'],
+    ['Diamond','water','Vacuum','Hydrogen','Vacuum'],
+    ['blue','black','white','red','black'],
+    ['hygrometer','hydrometer','manometer','Odometer','hygrometer'],
+    ['red,blue,yellow','red,blue,green','blue','yellow','green','red,yellow,green','red,blue,green'],
+    ['A','D','E','B','B'],
+    ['folic acid','retinol','frozen','none of these','folic acid'],
+    ['varicella','rubela','variola','vibrio Cholerae','vibrio Cholerae'],
+    ['chromium','Iron oxide','Silica','silicon','silicon'],
+    ['Globulin','Mucin','Keratin','Casein','Keratin'],
+    ['orange','Grapes','Apple','saffron','Grapes'],
+    ['Pokhran','Bombay','Nilli','Srihrikota','Pokhran'],
+    ['Mouth','Stomach','Small intestine','Large intestine','Small intestine'],
+    ['Gibberellins','Auxins','Cytokinins','florignes','Auxins'],
+    ['3','4','5','2','4'],
+    ['xerophytes','Halophytes','Epiphytes','Hydrophytes','Halophytes'],
+    ['U.S.A and canada','Britain and Switzwerland','France and Itally','Germany and Austria','Germany and Austria'],
+    ['Britain','Switzweland','India','U.S.A','U.S.A'],
+    ['1971','1975','1999','never','1975'],
+    ['in 1960s','in 1970s','in 1980s','in 1990s','in 1980s'],
+    ['1947','1950','1952','1956','1956'],
+
+]
+
+list1=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,]
+
+while True:
+    print('\n\n\n\n\nwellcome to the game if you wanna play click enter..')
+    print('for reading the rules type   "1"\n')
+    input1=input('enter your response:  ')
+    if input1.isspace() or len(input1)==0:
+        print('welcome to the game.........')
+        break
+    elif input1 == '1':
+        print('...............rulees..............\n'
+              +'1.the kbc game is simple you have \n'
+              +'   to answer cureectly of qyestions\n'
+              +'   for every currect option you get\n'
+              +'   fixed amount of money...........\n'
+              +'2.if you loss at once you loss the\n'
+              +'   game and all money you earn     \n'
+              +'   however if you reach to leavel 6,\n'
+              +'   and level 10,13 in this 3 stage ur\n'
+              +'   got fixed and after you get the \n'
+              +'   money of last stage you where   \n'
+              +'3.you can quite by that your game \n '
+              +'   will over and you get the money \n'
+              +'   acording to your last winning \n'
+              +'4.there are tottal two life line:-\n'
+              +'   a). 50%50-remove two wrong option\n'
+              +'   b). answer-show the currect answer\n'
+              +'   * after uisng answer life line \n'
+              +'     you cant use other life line  \n'
+              +'   * after using 50%50 life line  \n'
+              +'     you cant use other life till\n'
+              +'     you aret cross new stage....\n'
+              +'best of luck player :)              ')
+        noting=input('tap enter to continue')
+        print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        continue
+    else:
+        print('pleasee type someting in the given option\nanyyhing else is not valid input..')
+        nothing=input('tap enter to continue')
+        continue
+
+num=0
+winning_amt=000
+stage_amt=000
+fixing=1
+lol50=1
+lolans=1
+usess=True
+usess1=True
+ans=False
+usess11=True
+
+import random
+while True:
+    if fixing>3:
+        break
+    num+=1
+    
+    #logic to call the answer..................
+    question_selected=random.choice(list1)
+    index_question_selected=list1.index(question_selected)
+    list1.pop(index_question_selected)
+    question=list2[question_selected]
+    optiones=kbc_questions[question_selected]
+    answer=optiones[4]
+    
+    
+    while True:
+        print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        print('------------------------------------------------------------')
+        print('                  <<:::::Life Line:::::>>                  ')
+        print('     "7"=[_50/50_:{}]"                  "8"=[_answer_:{}]"'.format(lol50,lolans))
+        print('------------------------------------------------------------')
+        print('Q num {}.) {}'.format(num,question))
+        print('------------------------------------------------------------')
+        print('                   1.  {}\n                   2.  {}'.format(optiones[0],optiones[1]))
+        print('                   3.  {}\n                   4.  {}'.format(optiones[2],optiones[3]))
+        print('------------------------------------------------------------')
+        print('winning   amount   of   yours.......  : ',winning_amt)
+        print('stage amount of yours...............  : ',stage_amt)
+        print('------------------------------------------------------------')
+        print('[quite: "5"]                                    [rules: "6"]')
+        print('------------------------------------------------------------')
+        input2=input('enter your response here.....            ')
+        
+        if input2.isdigit():
+            if int(input2) in range(1,5):
+                if optiones[int(input2)-1]==answer:
+                    ans=True
+                else:
+                    print('\nyour answer is wrong you loss the game...')
+                    print('here is your winning amount..  ',stage_amt)
+                    ans=False
+                    fixing+=5
+                    break
+            elif int(input2)==5:
+                print('\nso you quite the game.... here is ur money...',winning_amt)
+                fixing+=5
+                break
+            elif int(input2)==6:
+                print('...............rulees..............\n'
+                    +'1.the kbc game is simple you have \n'
+                    +'   to answer cureectly of qyestions\n'
+                    +'   for every currect option you get\n'
+                    +'   fixed amount of money...........\n'
+                    +'2.if you loss at once you loss the\n'
+                    +'   game and all money you earn     \n'
+                    +'   however if you reach to leavel 6,\n'
+                    +'   and level 10,13 in this 3 stage ur\n'
+                    +'   got fixed and after you get the \n'
+                    +'   money of last stage you where   \n'
+                    +'3.you can quite by that your game \n '
+                    +'   will over and you get the money \n'
+                    +'   acording to your last winning \n'
+                    +'4.there are tottal two life line:-\n'
+                    +'   a). 50%50-remove two wrong option\n'
+                    +'   b). answer-show the currect answer\n'
+                    +'   * after uisng answer life line \n'
+                    +'     you cant use other life line  \n'
+                    +'   * after using 50%50 life line  \n'
+                    +'     you cant use other life till\n'
+                    +'     you aret cross new stage....\n'
+                    +'best of luck player :)              ')
+                noting=input('tap enter to continue')
+                print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+                continue
+            elif int(input2)==7:
+                if usess==True:
+                    print('\nactiveted 50%50... 2 wrong option were removed...')
+                    print('after that u can pick next life line when u reach stage')
+                    s=0
+                    while True:
+                        lol05=random.choice(optiones)
+                        if lol05==answer:
+                            pass
+                        else:
+                            index1=optiones.index(lol05)
+                            optiones.pop(index1)
+                            optiones.insert(index1,'----')
+                            s+=1
+                            lol50-=lol50
+                            lolans-=lolans
+                            if s==2:
+                                break
+                else:
+                    print('\nyou already used it............... you cant use it')
+                    nothing=input()
+                usess=False
+                usess1=False
+                usess11=True
+                continue
+            elif int(input2)==8:
+                if usess1==True:
+                    print('\nactivated "answer" life line... all wrong option were removed')
+                    print('you cant use any more life line......')
+                    k=0
+                    while True:
+                        lol055=random.choice(optiones)
+                        if lol055==answer:
+                            pass
+                        else:
+                            index11=optiones.index(lol055)
+                            optiones.pop(index11)
+                            optiones.insert(index11,'----')
+                            k+=1
+                            lol50-=lol50
+                            lolans-=lolans
+                            if k==5:
+                                break
+                else:
+                    print('you already used it.....')
+                    nothing=input()
+                usess1=False
+                usess=False
+                continue
+            else:
+                print('\nthere are only 8 options pleasse type number within 8')
+                nothing=input()
+                continue
+        else:
+            print('please dont type space and words')
+        
+        if ans==True:
+            amount=[0,1000,2000,3000,5000,10000,20000,40000,80000,160000,320000,640000,1250000,2550000,5000000,7500000,10000000,75000000]
+            winning_amt=amount[num]
+            print('\n\n\nyou won amount of................. $ ',winning_amt)
+            if num == 6:
+                stage_amt=amount[num]
+                print('\nyou got a stage amount if you loss you will get that stage amount $ ',stage_amt)
+                if usess11==True:
+                    usess1=True
+                    lolans+=1
+                    
+            elif num==10:
+                stage_amt=amount[num]
+                print('\nyou got a stage amount if you loss you will get that stage amount $ ',stage_amt)
+                if usess11==True:
+                    usess1=True
+                    lolans+=1
+            elif num==13:
+                stage_amt=amount[num]
+                print('\nyou got a stage amount if you loss you will get that stage amount $ ',stage_amt)
+                if usess11==True:
+                    usess1=True
+                    lolans+=1
+            if num>13:
+                print('you can quite now and win the amount $',winning_amt,'\notherwise if you loss you get the stage amount $',stage_amt)
+                
+            nothing=input('tap enter to continue')
+            break
